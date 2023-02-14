011fe03c2adde852c7867b7ecd3c18d8e88b9c68
Diana Yelemes <yelemessovaaaa@gmail.com>
2023-02-14 07:40:33 +0600

    Lab 3

---
+++ src/MoodChecker.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class MoodChecker
+{
+    public static void main(String[] args)
+    {
+        System.out.print("Enter a text: ");
+        String str= sc.nextLine();
+        nlpPipeline.init();
+        nlpPipeline.estimatingSentiment(text);
+    }
+}
